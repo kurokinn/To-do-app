18851e120cac859f602d4dd9f0c56fab2e36e459
EC2 Default User <ec2-user@ip-172-31-13-81.ap-southeast-1.compute.internal>
2018-11-12 10:53:23 +0000

    Changed root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'top#index'
-  
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'top#index', as: :authenticated_root
+    end
+    
+    unauthenticated :user do
+      root :to => 'devise/sessions#new', as: :unauthenticated_root
+    end
+    
+  end
     resources :list, only: %i(new create edit update destroy) do
       resources :card, except: %i(index)
   end
